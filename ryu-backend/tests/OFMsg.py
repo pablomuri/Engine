a9645763c84229512fe9671f0472403520f67f3f
Pablo Murillo <pablo.murillonogales@telefonica.com>
2016-04-22 09:44:37 +0200

    modificado: AdvancedProxyCore.py 	modificado: CompositionSpecification.xml 	modificado: OFMsg.py

--- ryu-backend/tests/OFMsg.py
+++ ryu-backend/tests/OFMsg.py
@@ -1,6 +1,13 @@
-class OFMsg(object):
-	"""docstring for ClassName"""
-	def __init__(self, arg):
-		super(ClassName, self).__init__()
-		self.arg = arg
+from ryu.ofproto import ofproto_v1_0 as ofproto
+from ryu.ofproto.ofproto_v1_0_parser import *
+
+class ofmsg(object):
+	"""docstring for ofmsg"""
+	def __init__(self, msg, dpid):
+		super(ofmsg, self).__init__()
+		version, msg_type, msg_len, xid = ryu.ofproto.ofproto_parser.header(msg)
+
+		if of_type == 10:
+			self.packetIn = OFPPacketIn.parser(dpid, version, msg_type, msg_len, xid, msg)
+
 		
